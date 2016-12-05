b04e5e5de3bf54689e0ff66407086fecb62bb534
Jungwoo Yang <jungwoo@scalgo.com>
2016-12-05 16:12:16 +0100

    Added todo..

--- cpp/trivially_copyable_tuple.cpp
+++ cpp/trivially_copyable_tuple.cpp
@@ -2,6 +2,9 @@
 #include <type_traits>
 #include <tuple>
 #include <utility>
+#include <typeinfo>
+
+// Reference: http://coliru.stacked-crooked.com/view?id=d51ff6c809c9d6fabede11d0fa67a19a-f0d9bbac4ab033ac5f4ce440d21735ee
 
 template <unsigned...>
 struct ReverseIntegerSequence {
@@ -44,6 +47,11 @@ struct TriviallyCopyableTuple
 	    : TriviallyCopyableTuple::BaseType(static_cast<Us &&>(us)...) {}
 
 	template <unsigned I, typename T>
+	static const T &_get(const TriviallyCopyableElement<I, T> &e) {
+		return e.value;
+	}
+
+	template <unsigned I, typename T>
 	static T &_get(TriviallyCopyableElement<I, T> &e) {
 		return e.value;
 	}
@@ -63,19 +71,39 @@ auto get(TriviallyCopyableTuple<Ts...> &tuple)
 
 template <unsigned I, typename T>
 struct TriviallyCopyableTupleElement {
-	typedef decltype(T::template _get<I>(T())) type;
+	// TODO: check or make it work with const element....
+	typedef typename std::decay<decltype(T::template _get<I>(T()))>::type type;
 };
 
 int main(int argc, const char *argv[]) {
+	std::cout << std::is_trivially_copyable<const int>::value
+	          << std::endl;
 	std::cout << std::is_trivially_copyable<std::tuple<int, int>>::value
 	          << std::endl;
 	std::cout << std::is_trivially_copyable<TriviallyCopyableTuple<int, float, double>>::value
 	          << std::endl;
-	std::cout << std::is_same<int, TriviallyCopyableTupleElement<1, TriviallyCopyableTuple<int, float, double>>>::value << std::endl;
-	std::cout << std::is_same<float, TriviallyCopyableTupleElement<1, TriviallyCopyableTuple<int, float, double>>>::value << std::endl;
+
+	std::cout << typeid(TriviallyCopyableTupleElement<0, TriviallyCopyableTuple<int, float, double>>::type).name() << std::endl;
+	std::cout << typeid(TriviallyCopyableTupleElement<1, TriviallyCopyableTuple<int, float, double>>::type).name() << std::endl;
+	std::cout << typeid(TriviallyCopyableTupleElement<2, TriviallyCopyableTuple<int, float, double>>::type).name() << std::endl;
+	std::cout << typeid(int).name() << " " << typeid(float).name() << " " << typeid(double).name() << std::endl;
+	std::cout << std::boolalpha;
+	std::cout << std::is_same<int, int>::value << std::endl;
+	std::cout << std::is_same<int, TriviallyCopyableTupleElement<0, TriviallyCopyableTuple<int, float, double>>::type>::value << std::endl;
+	std::cout << std::is_same<float, TriviallyCopyableTupleElement<1, TriviallyCopyableTuple<int, float, double>>::type>::value << std::endl;
+	std::cout << std::is_same<double, TriviallyCopyableTupleElement<2, TriviallyCopyableTuple<int, float, double>>::type>::value << std::endl;
 	TriviallyCopyableTuple<int, float, double> A;
+	const TriviallyCopyableTuple<int, float, double> B = {1,2.f,3.};
 	get<0>(A) = 1;
-	std::cout << get<0>(A) << std::endl;
+	get<1>(A) = 1.1;
+	get<2>(A) = 1.2;
+	std::cout << get<0>(A) << " " << get<1>(A) << " " << get<2>(A) << std::endl;
+	std::cout << get<0>(B) << " " << get<1>(B) << " " << get<2>(B) << std::endl;
+
+	std::tuple<const int, int> X(1,2);
+	std::cout << std::is_same<int, std::tuple_element<0, std::tuple<const int, int>>::type>::value << std::endl;
+	std::cout << std::is_same<int, std::tuple_element<1, std::tuple<const int, int>>::type>::value << std::endl;
+
 //	TriviallyCopyableTuple<std::tuple<int>> c; // error
 	return 0;
 }
